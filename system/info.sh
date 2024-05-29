0ac04f87c2216bdd8df4c6879f09fbbb29f69beb
LT-BACKEND <lunaticvpnscript293@gmail.com>
2024-05-29 17:23:29 +0700

    Update info.sh

--- system/info.sh
+++ system/info.sh
@@ -1,5 +1,5 @@
 clear
-figlet -f 3d "Gretongers Vpn Premium Tunneling" | lolcat
+figlet -f 3d "LUNATIC TUNNELING " | lolcat
 cat /root/log-install.txt
 echo "-----------------------------------------------------"
 echo "                Credit Script @LunaticTunnel" | lolcat
